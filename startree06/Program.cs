5d68bb61a2c02529abc1adcb130e419f10627da2
South_under_been <s2018s27@e-mirim.hs.kr>
2020-05-12 22:25:28 +0900

    startree_ 다중 for문을 통한 별 출력

--- startree06/Program.cs
+++ startree06/Program.cs
@@ -10,6 +10,19 @@ namespace startree06
     {
         static void Main(string[] args)
         {
+            int n = 8;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = n - 1; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
         }
     }
 }
